ca9f096b6fd81d44ad594f3a76b4e3548f2ee6d4
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2021-09-22 11:37:19 +0700

    Menampilkan tulisan selamat datang

---
+++ materi1/Tugas2_VezaAdiloviana.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas2 Veza Adiloviana</title>
+</head>
+<body>
+    <?php 
+    echo ("Selamat Datang <br>");
+    echo ("Ayo kita Belajar di Junior Web Developer bersama <br> di BPPTIK Cikarang");
+    ?>
+</body>
+</html>
